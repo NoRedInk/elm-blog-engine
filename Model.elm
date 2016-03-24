ff2eee1e8c8bc13363b320b27383b714c8396b3e
eeue56 <enalicho@gmail.com>
2016-03-24 01:35:39 +0100

    use authors

---
+++ Model.elm
@@ -0,0 +1,18 @@
+module Model where
+
+
+type alias Author =
+    { img : String
+    , fullName : String
+    , twitter : String
+    }
+
+
+authors =
+    { jeg2 =
+        { img = "https://cloud.githubusercontent.com/assets/1094080/12563301/9d84e346-c377-11e5-8546-433af08218c0.png"
+        , fullName = "James Edward Gray II"
+        , twitter = "JEG2"
+
+        }
+    }
